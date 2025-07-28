1fa200b529bcb196742d6d237cb46c0d050a96d7
Ibrahim Aslan <i.aslan2020@gtu.edu.tr>
2025-07-28 14:52:58 +0300

    ++

---
+++ FishnetNetworkingEvolved/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,39 @@
+using FishNet.Object;
+using FishNet.Object.Synchronizing;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : NetworkBehaviour
+{
+    public readonly SyncVar<Color> color = new SyncVar<Color>();
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        color.OnChange += OnColorChanged;
+        _spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
+    private void Update()
+    {
+        if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeColor();
+        }
+    }
+
+    [ServerRpc]
+    private void ChangeColor()
+    {
+        color.Value = Random.ColorHSV();
+    }
+
+    private void OnColorChanged(Color oldColor, Color newColor, bool asServer)
+    {
+        _spriteRenderer.color = color.Value;
+    }
+
+}
